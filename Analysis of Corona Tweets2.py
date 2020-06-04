08af608f0fc4c24470ed489058f359ccf8631384
Aise0 <65189091+Aise0@users.noreply.github.com>
2020-06-04 23:24:37 +0300

    Add files via upload

---
+++ Analysis of Corona Tweets2.py
@@ -0,0 +1,383 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[78]:
+
+
+import numpy as np
+import pandas as pd
+from collections import Counter
+import matplotlib.pyplot as plt
+
+
+# In[79]:
+
+
+pd.options.display.max_colwidth = 500
+
+
+# In[80]:
+
+
+f = pd.read_csv('202005171549_corona_tweets.csv')
+df = pd.DataFrame(data = f)
+df.head(3)
+
+
+# In[81]:
+
+
+f2 = pd.read_csv('202005171549_corona_tweets.csv', nrows=15)
+print(f2)
+
+
+# In[82]:
+
+
+#1- How many lines are there in the dataframe? How much space does it consume in your memory? 
+
+
+# In[83]:
+
+
+len(df)
+
+
+# In[84]:
+
+
+df.memory_usage()
+df.memory_usage(index=True).sum() #Overall memory consumption
+df.info(memory_usage='deep')
+
+
+# In[85]:
+
+
+#2- What are the column names?
+
+
+# In[86]:
+
+
+for col in df.columns: 
+    print(col) 
+
+
+# In[87]:
+
+
+#3- Identify the most tweeting 50 users.
+
+
+# In[88]:
+
+
+most_tweeting_users = df['screen_name'].value_counts() 
+print(most_tweeting_users.head(50))
+
+
+# In[89]:
+
+
+#4- Plot a histogram of the 50 most tweeting users.
+
+
+# In[90]:
+
+
+most_tweeting_users.head(50).hist(bins=50) 
+plt.title('Most Tweeting 50 Users')
+plt.xlabel('Number of tweets')
+plt.ylabel('Number of users')
+plt.show()
+
+
+# In[91]:
+
+
+#5- Identify duplicate tweets based on the 'text' field.
+
+
+# In[92]:
+
+
+duplicateRowsDF = df[df['text'].duplicated()]['text']
+print(duplicateRowsDF)
+
+
+# In[93]:
+
+
+# 6- Remove duplicate tweets based on the 'text' field. How many tweets do you have now?
+
+
+# In[94]:
+
+
+df.drop_duplicates(subset=['text'], inplace=False) 
+
+
+# In[95]:
+
+
+# 7- Replace all users names with the usrusr token. 
+#You need to write a regular expression that identify user names and use the replace methodto replace it with the 'usrusr' token.
+
+
+# In[96]:
+
+
+usernames = df['text'].str.findall(r'@([\w]+)')
+for item in usernames:
+    for element in item:
+        print(element)
+        
+        
+df['text'] = df['text'].replace(to_replace='@([\w]+)', value='usrusr', regex=True)
+
+
+# In[97]:
+
+
+#8- Replace all URLs with 'urlurl' token in the 'text' column.
+
+
+# In[98]:
+
+
+#urls = df['text'].str.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+#print(urls)
+
+df['text'] = df['text'].replace(to_replace='http[^\]*', value='urlurl', regex=True)
+
+
+# In[ ]:
+
+
+#9- If a tweet starts with "RT @", it is a retweet. Remove all retweets from the dataframe.
+
+
+# In[99]:
+
+
+df2 = df[~df.text.str.startswith('RT @')]
+df = df2
+
+
+# In[ ]:
+
+
+# 10- If the field is_retweet contains the value True, this tweet is a retweet as well. Remove those as well.
+
+
+# In[101]:
+
+
+retweets = df['is_retweet'] == True
+#df.drop(labels=retweets, inplace = False)
+df_without_retweets = df[~df.is_retweet == True]
+
+
+# In[ ]:
+
+
+# 11- Check how many duplicate tweets are there. Remove them. How many tweets do you have now?
+
+
+# In[100]:
+
+
+duplicateRowsDF = df.duplicated()
+df.drop_duplicates(subset=['is_retweet'], inplace=False) 
+
+
+# In[ ]:
+
+
+# 12- Plot a histogram of the text lengths. What is the length of the longest and the shortest tweets?
+
+
+# In[103]:
+
+
+length_of_tweets = df['text'].str.len()
+length_of_tweets.hist(bins=50) 
+plt.title('Text lengths of tweets')
+plt.xlabel('Number of tweets')
+plt.ylabel('Length of tweets')
+plt.show()
+
+
+# In[ ]:
+
+
+# 13- Calculate the average tweet length based on the 'text' field.
+
+
+# In[104]:
+
+
+length_of_tweets.mean()
+
+
+# In[ ]:
+
+
+#14- Extract all words from the 'text' field and put them in a new column. 
+#The findall method and a regular expression to recognize words will help you.
+
+
+# In[105]:
+
+
+words_of_tweets = df['text'].str.findall(r'\w+')
+#words_of_tweets = re.findall(r'\w+', df['text'].lower())
+df['Words'] = words_of_tweets
+print(df['Words'])
+
+
+# In[ ]:
+
+
+#15- Identify the words that occur the most. The Counter object from collections will help you.
+
+
+# In[106]:
+
+
+# empty Counter
+word_counter = Counter()
+#df['text'].str.findall(r'\w+')
+
+for tokenlist in df['text'].str.lower().str.findall(r'\w+'):
+    word_counter.update(tokenlist)
+#print(word_counter.most_common()[:200])
+print(word_counter.most_common())
+
+
+# In[ ]:
+
+
+# 16- How many of the words occur only once? List 100 of them. Provide tweets that contain these words.
+
+
+# In[107]:
+
+
+occur_once_keys = []
+for key, value in word_counter.items():
+    if value == 1:
+        occur_once_keys.append(key)
+        
+for item in occur_once_keys[:100]:
+    print(item)
+
+
+# In[ ]:
+
+
+#17- Find the longest 100 words. List them.
+
+
+# In[108]:
+
+
+new_word_counter = {}
+for k in sorted(word_counter, key=len, reverse=True): # Through keys sorted by length
+    new_word_counter[k] = word_counter[k]
+keys = list(new_word_counter.keys())
+for key in keys[0:100]:
+    print(key)
+
+
+# In[ ]:
+
+
+#18- Find tweets that are all in upper case, such as "THIS IS A TWEET ALL UPPER CASE." in their 'text' field. 
+
+
+# In[ ]:
+
+
+#df['text'].str.isupper()
+
+i = 0
+for i in df['text'][i]:
+    i +=1
+    if df['text'][i].str.isupper() == True:
+        print(df['text'][i])
+
+
+# In[ ]:
+
+
+#19- Identify the mostly occurring 50 hashtags.
+
+
+# In[109]:
+
+
+word_counter2 = Counter()
+for tokenlist in df['text'].str.lower().str.findall(r"#(\w+)"):
+    word_counter2.update(tokenlist)
+print(word_counter2.most_common()[:50])
+
+
+# In[ ]:
+
+
+#20- Export the final dataframe to csv and Excel files.
+
+
+# In[114]:
+
+
+df.to_csv(r'/Volumes/Archive/final\ assignment \finalassignment.csv', index = False)
+df.to_excel(r'/Volumes/Archive/final\ assignment \finalassignment.xlsx', index = False)
+
+
+# In[ ]:
+
+
+### Find tweets about Trump (not including retweets). Print 100 of them.
+
+
+# In[112]:
+
+
+tweets_trump =[]
+
+for item in df['text']:
+    if 'RT' not in item:
+        if 'Trump' in item:
+            tweets_trump.append(item)
+for item in tweets_trump[:100]:
+    print(item)
+    
+
+
+# In[ ]:
+
+
+### Find tweets about Turkey (not including retweets). Print 100 of them.
+
+
+# In[113]:
+
+
+tweets_turkey =[]
+
+for item in df['text'].str.lower():
+    if 'RT' not in item:
+        if 'turkey' in item:
+            tweets_turkey.append(item)
+for item in tweets_turkey[:100]:
+    print(item)
+
+
+# In[ ]:
+
+
+
+
--- README.md
+++ README.md
@@ -1,1 +1,102 @@
-# data-analysis-projects
+# Analysis of tweets about corona
+
+This project includes the analysis of tweets from "202005171549_corona_tweets.csv" file.
+The csv file is read into a Pandas Dataframe and perform the operations using Pandas.
+ 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+What things you need to install the software and how to install them:
+
+Python: https://realpython.com/installing-python
+
+```
+brew install python3
+```
+
+Additional: 
+Not an requisite but the best IDE to run the code is Jupyter Notebook
+
+To install Jupyter Notebook, go to https://jupyter.org/install
+
+
+### Installing
+
+To install pandas from PyPI:
+
+```
+pip install pandas
+```
+
+To install numpy:
+
+```
+pip install numpy
+```
+
+To install matplotlib:
+
+```
+pip install matplotlib
+```
+
+### Importing
+
+
+Pandas:
+
+```
+import pandas as pd
+```
+
+Numpy:
+
+
+```
+import numpy as np
+```
+
+Counter:
+
+```
+from collections import Counter
+```
+
+Matplotlib:
+
+```
+import matplotlib.pyplot as plt
+```
+
+### Examples
+
+Pandas:
+
+```
+f = pd.read_csv('202005171549_corona_tweets.csv')
+```
+
+Counter:
+
+```
+word_counter = Counter()
+```
+
+Matplotlib:
+
+```
+length_of_tweets.hist(bins=50) 
+plt.title('Text lengths of tweets')
+plt.xlabel('Number of tweets')
+plt.ylabel('Length of tweets')
+plt.show()
+```
+
+## Acknowledgments
+
+* Done as an assignment for Computational Social Sciences (SOCI354) course at Koç University.
+* Via Ali Hürriyetoglu's guidance and instructions.
